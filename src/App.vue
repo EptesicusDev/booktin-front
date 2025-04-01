63b1c592a17ea9bb7c37ee3218234090ebdfe5fd
unknown <EptesicusDev>
2025-04-01 14:19:48 +0200

    build: tailwind + claenup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <p class="p-6 bg-black">oe</p>
+  </header>
+
+  <RouterView />
+</template>
