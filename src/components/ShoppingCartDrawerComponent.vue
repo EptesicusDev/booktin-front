69fcede6ea28d283f055ea502870978ba927d396
levesqu221 <levesqu221@unicaen.fr>
2025-04-24 15:40:32 +0200

    build: translation

--- src/components/ShoppingCartDrawerComponent.vue
+++ src/components/ShoppingCartDrawerComponent.vue
@@ -23,11 +23,11 @@ const closePanel = () => {
             <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
               <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                 <div class="flex items-start justify-between">
-                  <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart</h2>
+                  <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Panier</h2>
                   <div class="ml-3 flex h-7 items-center">
                     <button @click="closePanel" type="button" class="relative -m-2 p-2 text-gray-400 hover:text-gray-500">
                       <span class="absolute -inset-0.5"></span>
-                      <span class="sr-only">Close panel</span>
+                      <span class="sr-only">Fermer le panneau</span>
                       <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                       </svg>
@@ -41,20 +41,20 @@ const closePanel = () => {
                       <!-- Exemple d’article -->
                       <li class="flex py-6">
                         <div class="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
-                          <img src="https://tailwindcss.com/plus-assets/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Product image" class="size-full object-cover">
+                          <img src="https://tailwindcss.com/plus-assets/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Image du produit" class="size-full object-cover">
                         </div>
                         <div class="ml-4 flex flex-1 flex-col">
                           <div>
                             <div class="flex justify-between text-base font-medium text-gray-900">
-                              <h3><a href="#">Throwback Hip Bag</a></h3>
-                              <p class="ml-4">$90.00</p>
+                              <h3><a href="#">Sac banane rétro</a></h3>
+                              <p class="ml-4">90,00 $</p>
                             </div>
-                            <p class="mt-1 text-sm text-gray-500">Salmon</p>
+                            <p class="mt-1 text-sm text-gray-500">Saumon</p>
                           </div>
                           <div class="flex flex-1 items-end justify-between text-sm">
-                            <p class="text-gray-500">Qty 1</p>
+                            <p class="text-gray-500">Qté 1</p>
                             <div class="flex">
-                              <button type="button" class="font-medium text-primary hover:hover-text-primary">Remove</button>
+                              <button type="button" class="font-medium text-primary hover:hover-text-primary">Supprimer</button>
                             </div>
                           </div>
                         </div>
@@ -66,18 +66,18 @@ const closePanel = () => {
 
               <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
                 <div class="flex justify-between text-base font-medium text-gray-900">
-                  <p>Subtotal</p>
-                  <p>$122.00</p>
+                  <p>Sous-total</p>
+                  <p>122,00 $</p>
                 </div>
-                <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                <p class="mt-0.5 text-sm text-gray-500">Les frais de livraison et les taxes seront calculés lors du paiement.</p>
                 <div class="mt-6">
-                  <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-primary px-6 py-3 text-base font-medium text-white shadow-xs hover:hover-bg-primary">Checkout</a>
+                  <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-primary px-6 py-3 text-base font-medium text-white shadow-xs hover:hover-bg-primary">Passer à la caisse</a>
                 </div>
                 <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
                   <p>
-                    or
+                    ou
                     <button @click="closePanel" type="button" class="font-medium text-primary hover:hoverhover-text-primary">
-                      Continue Shopping
+                      Continuer vos achats
                       <span aria-hidden="true"> &rarr;</span>
                     </button>
                   </p>
@@ -90,3 +90,4 @@ const closePanel = () => {
     </div>
   </div>
 </template>
+
